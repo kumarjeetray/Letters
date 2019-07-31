590af03dec994e461da6476f57dc469978014a78
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-07-31 22:37:29 +0530

    Add files via upload

---
+++ K.go
@@ -0,0 +1,15 @@
+package Pattern
+import "fmt"
+func main(){
+	a:=5
+	for i:=1í<=7;i++{
+		for j:=1;j<=7;j++{
+			if ((j==1)|| (j==a))
+			{
+				printf("*")
+			}else{
+				printf(" ")
+			}
+		}
+	}
+}
