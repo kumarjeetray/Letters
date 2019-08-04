fb031b0ef04c3dc7ca42e2e8041be2b849615f14
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-08-05 00:05:10 +0530

    Add files via upload

---
+++ M.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+func main(){
+	n:=2;
+	n2:=8
+	for i:=1;i<=7;i++{
+		for j:=1;j<=9;j++{
+			if (j==1 || j==9 || j==n||j==n2){
+				fmt.Print("*")
+			}else{
+				fmt.Print(" ")
+			}
+		}
+		if i<4{
+		n++
+		n2--
+		}else{
+			n=0
+			n2=0
+		}
+		fmt.Println()
+	}
+}
