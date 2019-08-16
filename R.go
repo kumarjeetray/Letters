715afb4f4d4861d5216194aba7ab34bf9d9a4605
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-08-16 18:33:03 +0530

    Add files via upload

---
+++ R.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func main(){
+	a:=4
+	for i:=1;i<=7;i++{
+		for j:=1;j<=7;j++{
+			if (i==1 ||i==4 ||j==1||j==7 && i<=4){
+				fmt.Print("*")
+				}else if (i>4 && j==a){
+					fmt.Print("*")
+					}else{
+				fmt.Print(" ")
+			}
+		}
+		if i>4{
+			a++
+		}
+		fmt.Println()	
+		}
+}
